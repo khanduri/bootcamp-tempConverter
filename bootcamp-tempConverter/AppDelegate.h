1a7273c565dadfae65b708c9fa8e14ea92274bb9
PrashantKhanduri-local <prashant.khanduri@gmail.com>
2013-10-02 21:33:13 -0700

    Initial Commit

---
+++ bootcamp-tempConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bootcamp-tempConverter
+//
+//  Created by Prashant Khanduri - Hearsay on 10/2/13.
+//  Copyright (c) 2013 KDeal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bootcamp-tempConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bootcamp-tempConverter
+//
+//  Created by Prashant Khanduri - Hearsay on 10/2/13.
+//  Copyright (c) 2013 KDeal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
